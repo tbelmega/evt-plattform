b6cc9da36a97d721c4a8a13ce647ce61b7b9ce8b
majab <maja.belmega@gmail.com>
2017-09-27 14:36:46 +0200

    Add TransportOffers

---
+++ src/main/java/de/belmega/eventers/user/services/TransportOffers.java
@@ -0,0 +1,39 @@
+package de.belmega.eventers.user.services;
+
+/**
+ * Created by majab on 27.09.2017.
+ */
+public class TransportOffers {
+    private String name;
+    private boolean enabled;
+    private String id;
+
+    public TransportOffers(String id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ src/main/java/de/belmega/eventers/user/services/transportOffersServicesBean.java
@@ -0,0 +1,45 @@
+package de.belmega.eventers.user.services;
+
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Created by majab on 26.09.2017.
+ */
+@Named
+@SessionScoped
+public class transportOffersServicesBean {
+
+    private List<String> selectedTransportOfferByUser = new ArrayList<>();
+
+    // Since the h:datatable wants to access the .name and .id and .enabled of each list element, it needs to be a full object instead of as string.
+    // Hence the class EntertainmentOffer
+    private List<TransportOffers> allAvailableTransportOffers = Arrays.asList(
+            new TransportOffers("taxi-service", "Taxi-Service"),
+            new TransportOffers("limousine-service", "Limosinen-Service"),
+            new TransportOffers("velo-taxi", "Velotaxi-Service"),
+            new TransportOffers("bicycle-rentals", "Fahrradverleih"));
+
+
+    public void setSelectedTransportOfferByUser(List<String> selectedTransportOfferByUser) {
+        this.selectedTransportOfferByUser = selectedTransportOfferByUser;
+    }
+
+    public List<String> getSelectedTransportOfferByUser() {
+        return selectedTransportOfferByUser;
+    }
+
+    public void setAllAvailableTransportOffers(List<TransportOffers> allAvailableCulturalOffers) {
+        this.allAvailableTransportOffers = allAvailableCulturalOffers;
+    }
+
+    public List<TransportOffers> getAllAvailableTransportOffers() {
+        return allAvailableTransportOffers;
+    }
+
+
+}
